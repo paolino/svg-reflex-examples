43d71dce4b6facbbf8ad6366b9121a59899aaf03
paolino <paolo.veronelli@gmail.com>
2016-10-09 21:53:55 +0200

    button

--- svg.hs
+++ svg.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE FlexibleContexts       #-}
 {-# LANGUAGE NoMonomorphismRestriction       #-}
+{-# LANGUAGE ConstraintKinds       #-}
 
 module Main where
 
@@ -11,11 +12,40 @@ import           Data.FileEmbed
 import           Data.Map       (Map)
 import qualified Data.Map as M
 import           Reflex.Dom
-import Data.Text
+import Data.Text (Text,pack)
+import Data.Monoid
+
+type SG = SpiderTimeline Global
+type DS = Dynamic SG
+type ES = Event SG
+type MS = MonadWidget SG
 
 ooo = return ()
-svg = elDynAttrNS' (Just $ "http://www.w3.org/2000/svg")
+
+
+main :: IO ()
+main = mainWidgetWithCss $(embedFile "style.css") $ do
+    el "body" $ do
+        divClass "svgDiv" $ do
+            el "h3" $ text "SVG"
+            svgApp
+        divClass "buttonDiv" $ do
+            el "h3" $ text "Button"
+            buttonApp
+
+
+--------------------- svg --------------------------------
+svgApp :: MS m => m ()
+svgApp = svg "svg" (pure svgA) $ do
+                svg "rect" (pure rectA) $ ooo
+                svg "circle" (pure circleA) $ ooo
+                svg "text" (pure textA) $ text "SVG"
+
+svg :: MS m => Text -> DS (Map Text Text) -> m () -> m ()
+svg n a r = elDynAttrNS' (Just $ "http://www.w3.org/2000/svg") n a r >> ooo
+
 svgA, rectA, circleA, textA :: Map Text Text 
+
 svgA = [ ("version", "1.1")
          , ("baseProfile", "full")
          , ("width", "300px")
@@ -27,11 +57,25 @@ circleA = [("cx", "150"), ("cy", "100"), ("r", "80"),
         ("fill", "green")]
 textA = [("x", "150"), ("y", "125"), ("font-size", "60"), 
         ("text-anchor", "middle"), ("fill", "white")]
-main :: IO ()
--- main = mainWidgetWithCss $(embedFile "style.css") atompit
-main = mainWidget $ do
-    svg "svg" (pure svgA) $ do
-        svg "rect" (pure rectA) $ ooo
-        svg "circle" (pure circleA) $ ooo
-        svg "text" (pure textA) $ text "SVG"
-    return ()
+
+-------------------- button ---------------------
+data Color = White | Red deriving Show
+
+touchMouse x y w = leftmost 
+        [   x <$ domEvent Mousedown w
+        ,   y <$ domEvent Mouseup w
+        ,   x <$ domEvent Touchstart w
+        ,   y <$ domEvent Touchend w]
+
+light :: MS m => DS Color ->  m ()
+light color = do
+    let attrs = fmap (\c -> [("style","background-color:" <> pack (show c)),("class","light")] :: Map Text Text) color
+
+    elDynAttr "div" attrs $ ooo
+
+buttonApp :: MS m => m ()
+buttonApp = do
+    btn <- fmap fst . el' "button" $ text "light"
+    color <- holdDyn White $ touchMouse Red White btn
+    light color
+
